d5fc2edc55608ed101a589ba3c8f41e8a543a1c5
cypermen <andreybabich2012@yandex.ru>
2019-12-15 02:13:55 +0300

    Логика переходов между главной активностью и соседними
    Описал кнопки из StartMenu активити в Autorization активити и обратно,и кнопку перехода в BuyerMenu активити

--- app/src/main/java/com/example/shop/BuyerMenu.java
+++ app/src/main/java/com/example/shop/BuyerMenu.java
@@ -1,14 +1,13 @@
 package com.example.shop;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class BuyerMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_startmenu);
+        setContentView(R.layout.activity_buyermenu);
     }
 }
