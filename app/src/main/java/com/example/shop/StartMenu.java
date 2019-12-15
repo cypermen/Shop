4a8170dba5c5a3b925817d7a30c41ed7ad32d360
Vadimochek <vadim-132@mail.ru>
2019-12-16 02:19:26 +0300

    Колоссальная работа в области потоков (Админ) Удалены некоторые классы.

--- app/src/main/java/com/example/shop/StartMenu.java
+++ app/src/main/java/com/example/shop/StartMenu.java
@@ -9,10 +9,33 @@ import android.widget.Button;
 
 public class StartMenu extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_buyermenu);
+        Button buyer;
+        Button admin;
+        Intent buyerIntention;
+        Intent adminIntention;
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_startmenu);
+            buyer = (Button) findViewById(R.id.start);
+            admin = (Button) findViewById(R.id.admin);
+            buyer.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    buyerIntention = new Intent(StartMenu.this, BuyerMenu.class);
+                    startActivity(buyerIntention);
+                }
+            });
+            admin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    adminIntention = new Intent(StartMenu.this, Authorization.class);
+                    startActivity(adminIntention);
+                }
+            });
+        }
 
     }
-}
+
