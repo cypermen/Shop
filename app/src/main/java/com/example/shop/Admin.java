8497b9491bc7cb49679e44880478fda45580b430
Unknown <vadim-132@mail.ru>
2019-12-16 13:04:39 +0300

    Обновление покупок клиентов

--- app/src/main/java/com/example/shop/Admin.java
+++ app/src/main/java/com/example/shop/Admin.java
@@ -39,28 +39,28 @@ public class Admin extends AppCompatActivity {
             public void handleMessage(android.os.Message msg) {
                 super.handleMessage(msg);
                 if (msg.what==1) {
-                    text.setText(text.getText().toString()+"Взято:  Игровая мышь1          ");
+                    text.setText(text.getText().toString()+"Взято: Монитор Acer Nitro VG271Pbmiipx              \n");
                     text.invalidate();
                 }if (msg.what==2){
-                    text.setText(text.getText().toString()+"Взято:  Игровая мышь2          ");
+                    text.setText(text.getText().toString()+"Взято:  Монитор LG 25UM58                             \n");
                     text.invalidate();}
                 if (msg.what==3){
-                    text.setText(text.getText().toString()+"Взято:  Игровая мышь3          ");
+                    text.setText(text.getText().toString()+"Взято:  Мышь Razer Basilisk Black USB                 \n");
                     text.invalidate();}
                 if (msg.what==4){
-                    text.setText(text.getText().toString()+"Взято:  Игровая мышь4          ");
+                    text.setText(text.getText().toString()+"Взято:  Мышь A4Tech Bloody V7                        \n");
                     text.invalidate();}
                 if (msg.what==5){
-                    text.setText(text.getText().toString()+"Взято:  Игровая мышь5          ");
+                    text.setText(text.getText().toString()+"Взято:  Компьютерная гарнитура Razer Kraken          \n");
                     text.invalidate();}
                 if (msg.what==6){
-                    text.setText(text.getText().toString()+"Взято:  Игровая мышь6          ");
+                    text.setText(text.getText().toString()+"Взято:  Компьютерная гарнитура HyperX Cloud Stinger    \n");
                     text.invalidate();}
                 if (msg.what==7){
-                    text.setText(text.getText().toString()+"Взято:  Игровая мышь7          ");
+                    text.setText(text.getText().toString()+"Взято:  Клавиатура Steelseries Apex 7 Red Switch      \n");
                     text.invalidate();}
                 if (msg.what==8){
-                    text.setText(text.getText().toString()+"Взято:  Игровая мышь8          ");
+                    text.setText(text.getText().toString()+"Взято:  Клавиатура HyperX Alloy Elite                  \n");
                     text.invalidate();}
                 if (msg.what==0){
                     text.setText(text.getText().toString()+"Идет на кассу...          ");
@@ -73,28 +73,28 @@ public class Admin extends AppCompatActivity {
             public void handleMessage(android.os.Message msg) {
                 super.handleMessage(msg);
                 if (msg.what==1) {
-                    text1.setText(text1.getText().toString()+"Взято:  Игровая мышь1         ");
+                    text1.setText(text1.getText().toString()+"Взято: Монитор Acer Nitro VG271Pbmiipx \n");
                     text1.invalidate();
                 }if (msg.what==2){
-                    text1.setText(text1.getText().toString()+"Взято:  Игровая мышь2         ");
+                    text1.setText(text1.getText().toString()+"Взято:  Монитор LG 25UM58             \n");
                     text1.invalidate();}
                 if (msg.what==3){
-                    text1.setText(text1.getText().toString()+"Взято:  Игровая мышь3         ");
+                    text1.setText(text1.getText().toString()+"Взято:  Мышь Razer Basilisk Black USB \n");
                     text1.invalidate();}
                 if (msg.what==4){
-                    text1.setText(text1.getText().toString()+"Взято:  Игровая мышь4         ");
+                    text1.setText(text1.getText().toString()+"Взято:  Мышь A4Tech Bloody V7         \n");
                     text1.invalidate();}
                 if (msg.what==5){
-                    text1.setText(text1.getText().toString()+"Взято:  Игровая мышь5         ");
+                    text1.setText(text1.getText().toString()+"Взято:  Компьютерная гарнитура Razer Kraken \n");
                     text1.invalidate();}
                 if (msg.what==6){
-                    text1.setText(text1.getText().toString()+"Взято:  Игровая мышь6         ");
+                    text1.setText(text1.getText().toString()+"Взято:  Компьютерная гарнитура HyperX Cloud Stinger \n");
                     text1.invalidate();}
                 if (msg.what==7){
-                    text1.setText(text1.getText().toString()+"Взято:  Игровая мышь7         ");
+                    text1.setText(text1.getText().toString()+"Взято:  Клавиатура Steelseries Apex 7 Red Switch \n");
                     text1.invalidate();}
                 if (msg.what==8){
-                    text1.setText(text1.getText().toString()+"Взято:  Игровая мышь8         ");
+                    text1.setText(text1.getText().toString()+"Взято:  Клавиатура HyperX Alloy Elite          \n");
                     text1.invalidate();}
                 if (msg.what==0){
                     text1.setText(text1.getText().toString()+"Идет на кассу...          ");
@@ -108,28 +108,28 @@ public class Admin extends AppCompatActivity {
             public void handleMessage(android.os.Message msg) {
                 super.handleMessage(msg);
                 if (msg.what==1) {
-                    text2.setText(text2.getText().toString()+"Взято:  Игровая мышь1          ");
+                    text2.setText(text2.getText().toString()+"Взято: Монитор Acer Nitro VG271Pbmiipx \n");
                     text2.invalidate();
                 }if (msg.what==2){
-                    text2.setText(text2.getText().toString()+"Взято:  Игровая мышь2          ");
+                    text2.setText(text2.getText().toString()+"Взято:  Монитор LG 25UM58             \n");
                     text2.invalidate();}
                 if (msg.what==3){
-                    text2.setText(text2.getText().toString()+"Взято:  Игровая мышь3          ");
+                    text2.setText(text2.getText().toString()+"Взято:  Мышь Razer Basilisk Black USB \n");
                     text2.invalidate();}
                 if (msg.what==4){
-                    text2.setText(text2.getText().toString()+"Взято:  Игровая мышь4          ");
+                    text2.setText(text2.getText().toString()+"Взято:  Мышь A4Tech Bloody V7         \n");
                     text2.invalidate();}
                 if (msg.what==5){
-                    text2.setText(text2.getText().toString()+"Взято:  Игровая мышь5          ");
+                    text2.setText(text2.getText().toString()+"Взято:  Компьютерная гарнитура Razer Kraken \n");
                     text2.invalidate();}
                 if (msg.what==6){
-                    text2.setText(text2.getText().toString()+"Взято:  Игровая мышь6          ");
+                    text2.setText(text2.getText().toString()+"Взято:  Компьютерная гарнитура HyperX Cloud Stinger \n");
                     text2.invalidate();}
                 if (msg.what==7){
-                    text2.setText(text2.getText().toString()+"Взято:  Игровая мышь7          ");
+                    text2.setText(text2.getText().toString()+"Взято:  Клавиатура Steelseries Apex 7 Red Switch \n");
                     text2.invalidate();}
                 if (msg.what==8){
-                    text2.setText(text2.getText().toString()+"Взято:  Игровая мышь8          ");
+                    text2.setText(text2.getText().toString()+"Взято:  Клавиатура HyperX Alloy Elite          \n");
                     text2.invalidate();}
                 if (msg.what==0){
                     text2.setText(text2.getText().toString()+"Идет на кассу...          ");
@@ -142,28 +142,28 @@ public class Admin extends AppCompatActivity {
             public void handleMessage(android.os.Message msg) {
                 super.handleMessage(msg);
                 if (msg.what==1) {
-                    text3.setText(text3.getText().toString()+"Взято:  Игровая мышь1          ");
+                    text3.setText(text3.getText().toString()+"Взято: Монитор Acer Nitro VG271Pbmiipx \n");
                     text3.invalidate();
                 }if (msg.what==2){
-                    text3.setText(text3.getText().toString()+"Взято:  Игровая мышь2          ");
+                    text3.setText(text3.getText().toString()+"Взято:  Монитор LG 25UM58             \n");
                     text3.invalidate();}
                 if (msg.what==3){
-                    text3.setText(text3.getText().toString()+"Взято:  Игровая мышь3          ");
+                    text3.setText(text3.getText().toString()+"Взято:  Мышь Razer Basilisk Black USB \n");
                     text3.invalidate();}
                 if (msg.what==4){
-                    text3.setText(text3.getText().toString()+"Взято:  Игровая мышь4          ");
+                    text3.setText(text3.getText().toString()+"Взято:  Мышь A4Tech Bloody V7         \n");
                     text3.invalidate();}
                 if (msg.what==5){
-                    text3.setText(text3.getText().toString()+"Взято:  Игровая мышь5          ");
+                    text3.setText(text3.getText().toString()+"Взято:  Компьютерная гарнитура Razer Kraken \n");
                     text3.invalidate();}
                 if (msg.what==6){
-                    text3.setText(text3.getText().toString()+"Взято:  Игровая мышь6          ");
+                    text3.setText(text3.getText().toString()+"Взято:  Компьютерная гарнитура HyperX Cloud Stinger \n");
                     text3.invalidate();}
                 if (msg.what==7){
-                    text3.setText(text3.getText().toString()+"Взято:  Игровая мышь7          ");
+                    text3.setText(text3.getText().toString()+"Взято:  Клавиатура Steelseries Apex 7 Red Switch \n");
                     text3.invalidate();}
                 if (msg.what==8){
-                    text3.setText(text3.getText().toString()+"Взято:  Игровая мышь8          ");
+                    text3.setText(text3.getText().toString()+"Взято:  Клавиатура HyperX Alloy Elite          \n");
                     text3.invalidate();}
                 if (msg.what==0){
                     text3.setText(text3.getText().toString()+"Идет на кассу...          ");
@@ -176,28 +176,28 @@ public class Admin extends AppCompatActivity {
             public void handleMessage(android.os.Message msg) {
                 super.handleMessage(msg);
                 if (msg.what==1) {
-                    text4.setText(text4.getText().toString()+"Взято:  Игровая мышь1          ");
+                    text4.setText(text4.getText().toString()+"Взято: Монитор Acer Nitro VG271Pbmiipx \n");
                     text4.invalidate();
                 }if (msg.what==2){
-                    text4.setText(text4.getText().toString()+"Взято:  Игровая мышь2          ");
+                    text4.setText(text4.getText().toString()+"Взято:  Монитор LG 25UM58             \n");
                     text4.invalidate();}
                 if (msg.what==3){
-                    text4.setText(text4.getText().toString()+"Взято:  Игровая мышь3          ");
+                    text4.setText(text4.getText().toString()+"Взято:  Мышь Razer Basilisk Black USB \n");
                     text4.invalidate();}
                 if (msg.what==4){
-                    text4.setText(text4.getText().toString()+"Взято:  Игровая мышь4          ");
+                    text4.setText(text4.getText().toString()+"Взято:  Мышь A4Tech Bloody V7         \n");
                     text4.invalidate();}
                 if (msg.what==5){
-                    text4.setText(text4.getText().toString()+"Взято:  Игровая мышь5          ");
+                    text4.setText(text4.getText().toString()+"Взято:  Компьютерная гарнитура Razer Kraken \n");
                     text4.invalidate();}
                 if (msg.what==6){
-                    text4.setText(text4.getText().toString()+"Взято:  Игровая мышь6          ");
+                    text4.setText(text4.getText().toString()+"Взято:  Компьютерная гарнитура HyperX Cloud Stinger \n");
                     text4.invalidate();}
                 if (msg.what==7){
-                    text4.setText(text4.getText().toString()+"Взято:  Игровая мышь7          ");
+                    text4.setText(text4.getText().toString()+"Взято:  Клавиатура Steelseries Apex 7 Red Switch \n");
                     text4.invalidate();}
                 if (msg.what==8){
-                    text4.setText(text4.getText().toString()+"Взято:  Игровая мышь8          ");
+                    text4.setText(text4.getText().toString()+"Взято:  Клавиатура HyperX Alloy Elite          \n");
                     text4.invalidate();}
                 if (msg.what==0){
                     text4.setText(text4.getText().toString()+"Идет на кассу...          ");
@@ -230,7 +230,7 @@ public class Admin extends AppCompatActivity {
     }
 
     class AnotherRunnable implements Runnable {
-        private int x = random.nextInt(10) + 1; // Cоздание х циклов в потоке
+        private int x = random.nextInt(10) + 2; // Cоздание х циклов в потоке
         @Override
         public void run() {
             for (int i = 0; i < x; i++) {
@@ -252,7 +252,7 @@ public class Admin extends AppCompatActivity {
         }
     }
     class AnotherRunnable1 implements Runnable {
-        private int x = random.nextInt(10) + 1;
+        private int x = random.nextInt(10) + 2;
         @Override
         public void run() {
             for (int i = 0; i < x; i++) {
@@ -274,7 +274,7 @@ public class Admin extends AppCompatActivity {
         }
     }
     class AnotherRunnable2 implements Runnable {
-        private int x = random.nextInt(10) + 1;
+        private int x = random.nextInt(10) + 2;
         @Override
         public void run() {
             for (int i = 0; i < x; i++) {
@@ -296,7 +296,7 @@ public class Admin extends AppCompatActivity {
         }
     }
     class AnotherRunnable3 implements Runnable {
-        private int x = random.nextInt(10) + 1;
+        private int x = random.nextInt(10) + 2;
         @Override
         public void run() {
             for (int i = 0; i < x; i++) {
@@ -317,7 +317,7 @@ public class Admin extends AppCompatActivity {
         }
     }
     class AnotherRunnable4 implements Runnable {
-        private int x = random.nextInt(10) + 1;
+        private int x = random.nextInt(10) + 2;
         @Override
         public void run() {
             for (int i = 0; i < x; i++) {
